15630b270d3eaa4b5d70f62468d3fa19a8e90fdf
Alec Smrekar <alec.smrekar@gmail.com>
2021-03-28 17:49:53 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Web.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Web.dir/main.c.o"
+  "Web"
+  "Web.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Web.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
