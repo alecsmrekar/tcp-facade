3683caf65b683e1774a418c75d9bf2b0be6f9fe1
Alec Smrekar <alec.smrekar@gmail.com>
2021-03-30 22:23:40 +0200

    Added nonblocking io sample

--- main.c
+++ main.c
@@ -9,9 +9,19 @@
 #include<netdb.h>
 #include<signal.h>
 #include<fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <unistd.h>
+#include<stdio.h>
+#include<sys/time.h>
+
 
 #define CONNMAX 2
 #define BYTES 1024
+#define NB_DISABLE 1
+#define NB_ENABLE 0
 
 
 // https://www.csd.uoc.gr/~hy556/material/tutorials/cs556-3rd-tutorial.pdf
@@ -29,7 +39,65 @@ void startServer(char *);
 
 void consoleRespond(int);
 
+//https://github.com/zrafa/onscreenkeyboard/blob/master/key.c
+int kbhit()
+{
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+void nonblock(int state)
+{
+    struct termios ttystate;
+
+    //get the terminal state
+    tcgetattr(STDIN_FILENO, &ttystate);
+
+    if (state==NB_ENABLE)
+    {
+        //turn off canonical mode
+        ttystate.c_lflag &= ~ICANON;
+        //minimum of number input read.
+        ttystate.c_cc[VMIN] = 1;
+    }
+    else if (state==NB_DISABLE)
+    {
+        //turn on canonical mode
+        ttystate.c_lflag |= ICANON;
+    }
+    //set the terminal attributes.
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+
+}
+
 int main(int argc, char *argv[]) {
+    char ci;
+    int i=0;
+
+    nonblock(NB_ENABLE);
+    while(!i)
+    {
+        usleep(1);
+        i=kbhit();
+        if (i!=0)
+        {
+            ci=fgetc(stdin);
+            if (ci=='q')
+                i=1;
+            else
+                i=0;
+        }
+    }
+    printf("\n you hit %c. \n",ci);
+    nonblock(NB_DISABLE);
+
+
     struct sockaddr_in clientaddr; // Structure describing an Internet socket address
     socklen_t addrlen;              // client address length
     char c;
